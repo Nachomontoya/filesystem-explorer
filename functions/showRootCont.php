a56b90a49ed7422cf278d0fda71cfb08c46c0cd7
Nachomontoya <montoyanacho@gmail.com>
2021-06-23 12:40:28 +0200

    linted

--- functions/showRootCont.php
+++ functions/showRootCont.php
@@ -1,33 +1,34 @@
 <?php
 
 
-    function showRootContent(){
-        $files = array();
+function showRootContent()
+{
+    $files = array();
 
-        $path = getcwd();
-        chdir('root/');
-        $path = getcwd();
-        if($gestor = opendir($path)){
-            while($archivo = readdir($gestor)){
-            if($archivo != '.' && $archivo != '..'){
+    $path = getcwd();
+    chdir('root/');
+    $path = getcwd();
+    if ($gestor = opendir($path)) {
+        while ($archivo = readdir($gestor)) {
+            if ($archivo != '.' && $archivo != '..') {
                 // echo "nombre archivo: $archivo : tipo archivo: " . filetype($path.'/' . $archivo) . "<br>";
                 $files[] = $archivo;
             }
         }
         closedir($gestor);
     }
-    foreach ($files as $file ) {
-        if(is_dir($file)){
-        echo '<li class="nav-item">';
-        echo '<a class="nav-link collapsed" href="#">';
-        echo '<i class="fas fa-fw fa-folder"></i>';
-        echo '<span>'.$file.'</span>';
-        echo '</a>';
-      echo '</li>';
+    foreach ($files as $file) {
+        if (is_dir($file)) {
+            echo '<li class="nav-item">';
+            echo '<a class="nav-link collapsed" href="#">';
+            echo '<i class="fas fa-fw fa-folder"></i>';
+            echo '<span>' . $file . '</span>';
+            echo '</a>';
+            echo '</li>';
 
-    //    Divider
-     echo '<hr class="sidebar-divider d-none d-md-block" />';
+            //    Divider
+            echo '<hr class="sidebar-divider d-none d-md-block" />';
         }
     }
     return $files;
-    }
+}
