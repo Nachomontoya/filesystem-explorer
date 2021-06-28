8e47f2ceddc2f2214ecdd68e4ad6f79ad0b0b499
Ethan <ctabbx@gmail.com>
2021-06-28 13:16:07 +0200

    Delete Files Branch

--- functions/upload.php
+++ functions/upload.php
@@ -3,12 +3,12 @@
 $file = $_FILES['file'];
 $fileName = $file['name'];
 $fileType = $file['type'];
-$root = '../root';
+$path = '../root';
 if(!is_dir('../root')){
     opendir('../root',0777);
 }else{
     opendir('../root',0777);
-    move_uploaded_file($file['tmp_name'],"$root/$fileName");
+    move_uploaded_file($file['tmp_name'],"$path/$fileName");
 }
 header('Location: ../index.php');
 
