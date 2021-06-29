810743f155bae31381c88604dcb31ea1ba66bce6
Nachomontoya <montoyanacho@gmail.com>
2021-06-29 10:32:12 +0200

    upload fixed

--- functions/upload.php
+++ functions/upload.php
@@ -6,11 +6,11 @@ $fileType = $file['type'];
 $rootPath = '../root';
 $currentPath = $_SESSION['path'];
 
-
 if($currentPath == NULL){
     move_uploaded_file($file['tmp_name'],"$rootPath/$fileName");
+    echo $currentPath;
 }else{
-    chdir($rootPath.'/'.$currentPath);
+    chdir($currentPath);
     $actualPath = getcwd();
     move_uploaded_file($file['tmp_name'],"$actualPath/$fileName");
 }
--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-// session_start();
 require './functions/showRootCont.php';
 
 $rootPath = getRootPath();
+
 
 ?>
 
